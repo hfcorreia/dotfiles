9150779eaa813db19a96b947f1c3b160c1266bff
Hugo Correia <hfcorreia91@gmail.com>
2015-10-14 14:16:03 +0100

    Move env variables to .localrc

--- system/env.zsh
+++ system/env.zsh
@@ -1,11 +1,9 @@
-# Env variables
-export ANDROID_HOME="/usr/local/opt/android-sdk"
-export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)
-export JRUBY_OPTS="-J-XX:+CMSClassUnloadingEnabled -J-XX:+UseConcMarkSweepGC -J-XX:+UseParNewGC -J-XX:MaxPermSize=256m -J-Xms1024m -J-Xmx1024m -Xcompile.invokedynamic=false"
-
 # Language
 export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8 
+export LC_ALL=en_US.UTF-8
 
 # Use vim as default editor
 export EDITOR="vim"
+
+# Define the default dev folder
+export DEV_DIR=$HOME/Development
