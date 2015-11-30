edb895e6278569208d65b47b191da47428c20788
Hugo Correia <hfcorreia91@gmail.com>
2015-11-30 14:28:38 +0000

    Add quick aliases for git

---
+++ git/alias.zsh
@@ -0,0 +1,8 @@
+# Git aliases based on the current branch
+alias gpush='git push origin $(git symbolic-ref --short -q HEAD)'
+alias gpull='git pull origin $(git symbolic-ref --short -q HEAD)'
+alias gstat='git st'
+
+# Quick change to known branches
+alias gmaster='git checkout master && gpull'
+alias gstaging='git checkout staging && gpull'
