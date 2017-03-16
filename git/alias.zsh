eed10e35226d57ad4c595c38f3c7d0934116d5b4
Hugo Correia <hfcorreia91@gmail.com>
2017-03-16 16:27:07 +0000

    Small updates

--- git/alias.zsh
+++ git/alias.zsh
@@ -9,5 +9,8 @@ alias gstaging='git checkout staging && gpull'
 # Quickly create a new branch
 alias gcreate='gmaster && git checkout -b '
 
+# Quickly set upstream
+alias gupstream='git branch --set-upstream-to=origin/$(git symbolic-ref --short -q HEAD) $(git symbolic-ref --short -q HEAD)'
+
 # Quick merges
 alias gmerge='git merge '
--- system/alias.zsh
+++ system/alias.zsh
@@ -2,3 +2,4 @@
 alias c="clear"
 alias e="exit"
 alias dev='cd $HOME/Development'
+alias dotfiles='cd $HOME/.dotfiles'
