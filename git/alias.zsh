852125b22a7038f9cb6e380e84200cb0d714ba60
Hugo Correia <hugo@hfcorreia.com>
2020-09-17 10:55:02 +0100

    Update settings

--- git/alias.zsh
+++ git/alias.zsh
@@ -7,12 +7,11 @@ alias gpull='git pull origin $(git symbolic-ref --short -q HEAD)'
 # Quickly set upstream
 alias gupstream='git branch --set-upstream-to=origin/$(git symbolic-ref --short -q HEAD) $(git symbolic-ref --short -q HEAD)'
 
-# Quick merges
-alias gmerge='git merge '
-
 # Quick toggle branch
 alias g-='git co -'
 
 alias amend!='git commit -a --amend --no-edit && git push --force-with-lease'
 
 alias glog='git --no-pager show -s --format=%B HEAD | pbcopy'
+
+alias grebase!='git co master && git pull origin master && git co - && git rebase -'
