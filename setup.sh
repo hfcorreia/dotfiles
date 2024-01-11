ae3aa11baeae0ec12d9c12a4c415734313d88262
Hugo Correia <hugo@hfcorreia.com>
2024-01-11 17:13:53 +0000

    Add setup

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Installing homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+echo "Cloning dotfiles to .dotfiles"
+git clone git@github.com:hfcorreia/dotfiles.git ~/.dotfiles
+
+echo "Running brew bundle"
+cd ~/.dotfiles
+brew bundle
