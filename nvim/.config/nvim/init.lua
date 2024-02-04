303a7c97f7439a61d2dbc830a2af8f65a7f8fee3
Hugo Correia <hugo@hfcorreia.com>
2024-02-04 11:24:15 +0000

    Add trouble plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -34,6 +34,10 @@ require("lazy").setup({
   "williamboman/mason.nvim",
   'vim-test/vim-test',
   {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+  {
     "L3MON4D3/LuaSnip",
     dependencies = { "rafamadriz/friendly-snippets" },
   },
@@ -328,3 +332,6 @@ vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+
+-- trouble vim
+require("trouble").setup({})
