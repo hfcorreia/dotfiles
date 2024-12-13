83f3ef63401781447b0290c4ad00a13c24dfaabe
Hugo Correia <hugo@hfcorreia.com>
2024-12-13 15:41:11 +0000

    Cleanup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -20,7 +20,6 @@ require("lazy").setup({
   "mbbill/undotree",
   "sindrets/diffview.nvim",
   "editorconfig/editorconfig-vim",
-  "ThePrimeagen/git-worktree.nvim",
   "github/copilot.vim",
   "shumphrey/fugitive-gitlab.vim",
   "bronson/vim-trailing-whitespace",
@@ -115,8 +114,6 @@ require("lazy").setup({
 vim.o.termguicolors = true
 vim.o.hlsearch = false
 vim.o.incsearch = true
-vim.opt.clipboard = "unnamedplus"
-vim.opt.mouse = "r"
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
@@ -134,10 +131,32 @@ vim.opt.scrolloff = 10
 vim.keymap.set("n", "<esc><esc>", ":noh<cr>")
 vim.keymap.set("i", "jk", "<esc>", { noremap = true })
 
+-- Usefull mappings
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({"n", "v"}, "<leader>d", "\"_d")
+
+-- super rename
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Undotree config
-vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle)
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
 
+-- Foramtting
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 -- Treesitter config
 require("nvim-treesitter.configs").setup {
@@ -165,12 +184,6 @@ require("nvim-treesitter.configs").setup {
     enable = true
   },
 }
-
--- trouble vim
-require("trouble").setup({
-
-
-})
 
 -- Telescope config
 require("telescope").setup({
@@ -380,15 +393,6 @@ require("catppuccin").setup({
   }
 })
 vim.cmd.colorscheme "catppuccin-mocha"
-
--- git worktree
-require("telescope").load_extension("git_worktree")
-
-require("git-worktree").setup({})
-
-vim.keymap.set("n", "<leader>gg", ":lua require('telescope').extensions.git_worktree.git_worktrees()<cr>", default_opts)
-vim.keymap.set("n", "<leader>gn", ":lua require('telescope').extensions.git_worktree.create_git_worktree()<cr>",
-  default_opts)
 
 -- harpoon
 local harpoon = require("harpoon")
