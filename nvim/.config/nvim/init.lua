b8d0e8b69b50d03021d12f149a2ea4b33261ef01
Hugo Correia <hugo@hfcorreia.com>
2024-12-17 09:52:59 +0000

    Update config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -44,6 +44,17 @@ require("lazy").setup({
   "williamboman/mason.nvim",
   "vim-test/vim-test",
   {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    }
+  },
+  {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {}, -- for default options, refer to the configuration section for custom setup.
@@ -144,10 +155,10 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", "\"_d")
+vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
 
 -- super rename
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
@@ -393,6 +404,27 @@ require("catppuccin").setup({
   }
 })
 vim.cmd.colorscheme "catppuccin-mocha"
+
+-- noice setup
+
+require("noice").setup({
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true,         -- use a classic bottom cmdline for search
+    command_palette = true,       -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false,       -- add a border to hover docs and signature help
+  },
+})
 
 -- harpoon
 local harpoon = require("harpoon")
